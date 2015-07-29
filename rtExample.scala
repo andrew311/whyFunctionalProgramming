950c7e388dc2941f378485cd319f543275c5a252
andrew311 <andrew311@gmail.com>
2015-07-29 16:23:22 -0400

    tabs to spaces

--- rtExample.scala
+++ rtExample.scala
@@ -1,30 +1,30 @@
 // non-RT
 
 class CandyMachine {
-	var candies: List[Candy] = List.empty()
-	
-	def addCandy(candy: Candy): Unit = {
-		candies = candy :: candies
-	}
-	
-	def giveMeCandy: Option[Candy] = {
-		candies match {
-			case head :: tail =>
-				candies = tail
-				head
-			case _ => None
-		}
-	}
+  var candies: List[Candy] = List.empty()
+  
+  def addCandy(candy: Candy): Unit = {
+    candies = candy :: candies
+  }
+  
+  def giveMeCandy: Option[Candy] = {
+    candies match {
+      case head :: tail =>
+        candies = tail
+        head
+      case _ => None
+    }
+  }
 }
 
 // RT
 
 case class CandyMachine(candies: List[Candy]) {
-	def addCandy(candy: Candy) {
-		CandyMachine(candy :: candies)
-	}
-	
-	def giveMeCandy: (Option[Candy], CandyMachine) = {
-		(candies.headOption, CandyMachine(candies.drop(1)))
-	}
+  def addCandy(candy: Candy) {
+    CandyMachine(candy :: candies)
+  }
+  
+  def giveMeCandy: (Option[Candy], CandyMachine) = {
+    (candies.headOption, CandyMachine(candies.drop(1)))
+  }
 }
